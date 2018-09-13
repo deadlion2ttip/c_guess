92cb9958af27b343fed7e9d4ccd546337662881c
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-09-13 13:55:18 -0400

    Set up try number.

--- c_guess/main.c
+++ c_guess/main.c
@@ -8,11 +8,15 @@ int main()
     int tries = 3;
 
     do {
-        printf("Make a guess:");
+        printf("You have %d tries left.\n", tries);
+        printf("Make a guess: ");
         scanf("%d", &guess);
-    } while(guess != secretNumber);
+        tries --;
+    } while(guess != secretNumber && tries > 0);
 
-
+    if (guess == secretNumber){
     printf("You win!");
+    } else {
+    printf("Out of tries, You lose!");}
     return 0;
 }
