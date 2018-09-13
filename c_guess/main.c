c8e8488e72a0a3fe9a374fc50efd0cf504ff2b23
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-09-13 13:55:45 -0400

    Random number now truely random.

--- c_guess/main.c
+++ c_guess/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+int random(int lower, int upper){
+int num = 5;
+num = (rand() % (upper - lower + 1)) + lower;
 
+return num;
+}
 int main()
 {
-    int secretNumber = 5;
+    int lower = 1;
+    int upper = 10;
+    srand(time(0));
+    int secretNumber = random(lower, upper);
+    printf("%d", secretNumber);
     int guess;
     int tries = 3;
 
